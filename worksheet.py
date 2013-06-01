776966338b2dc187f369fa249893c27440b06cac
James Cartledge <jcartledge@gmail.com>
2013-06-01 19:44:29 +1000

    status messages, timeouts are next

--- worksheet.py
+++ worksheet.py
@@ -8,6 +8,8 @@ class WorksheetCommand(sublime_plugin.TextCommand):
         self.repl = self.get_repl()
         if self.repl is not None:
             self.remove_previous_results()
+            self.view.set_status(
+                'worksheet', 'Initialising {0} REPL.'.format(self.language))
             self.repl.correspond('')
             self.process_line(0)
 
@@ -17,6 +19,7 @@ class WorksheetCommand(sublime_plugin.TextCommand):
         view_settings = self.view.settings()
         language = view_settings.get('syntax').split('/')[-1].split('.')[0]
         if language in languages:
+            self.language = language
             repl_settings = languages.get(language)
             args = repl_settings.get('args')
             del repl_settings['args']
@@ -42,6 +45,8 @@ class WorksheetCommand(sublime_plugin.TextCommand):
             line_text += "\n"
         thread = repl.ReplThread(self.repl, line_text)
         self.queue_thread(thread, next_start, is_last_line)
+        self.view.set_status(
+            'worksheet', 'Sending 1 line to {0} REPL.'.format(self.language))
         thread.start()
 
     def queue_thread(self, thread, start, is_last_line):
@@ -52,8 +57,11 @@ class WorksheetCommand(sublime_plugin.TextCommand):
 
     def handle_result(self, thread, next_start, is_last_line):
         if thread.is_alive():
+            self.view.set_status(
+                'worksheet', 'Waiting for {0} REPL.'.format(self.language))
             self.queue_thread(thread, next_start, is_last_line)
         else:
+            self.view.set_status('worksheet', '')
             edit = self.view.begin_edit('process_line')
             self.view.insert(edit, next_start, thread.result)
             self.view.end_edit(edit)
