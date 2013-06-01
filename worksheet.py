f2c0ba536194d6d4f597c37764ffd5abab116998
James Cartledge <jcartledge@gmail.com>
2013-06-01 19:56:56 +1000

    cleanup status

--- worksheet.py
+++ worksheet.py
@@ -8,24 +8,23 @@ class WorksheetCommand(sublime_plugin.TextCommand):
         self.repl = self.get_repl()
         if self.repl is not None:
             self.remove_previous_results()
-            self.view.set_status(
-                'worksheet', 'Initialising {0} REPL.'.format(self.language))
             self.repl.correspond('')
             self.process_line(0)
 
     def get_repl(self):
         settings = sublime.load_settings("worksheet.sublime-settings")
         languages = settings.get('worksheet_languages')
-        view_settings = self.view.settings()
-        language = view_settings.get('syntax').split('/')[-1].split('.')[0]
+        language = self.get_language()
         if language in languages:
-            self.language = language
             repl_settings = languages.get(language)
             args = repl_settings.get('args')
             del repl_settings['args']
             return repl.Repl(args, **repl_settings)
         else:
             sublime.error_message('No worksheet REPL found for ' + language)
+
+    def get_language(self):
+        return self.view.settings().get('syntax').split('/')[-1].split('.')[0]
 
     def remove_previous_results(self):
         view = self.view
@@ -45,26 +44,27 @@ class WorksheetCommand(sublime_plugin.TextCommand):
             line_text += "\n"
         thread = repl.ReplThread(self.repl, line_text)
         self.queue_thread(thread, next_start, is_last_line)
-        self.view.set_status(
-            'worksheet', 'Sending 1 line to {0} REPL.'.format(self.language))
+        self.set_status('Sending 1 line to %(language)s REPL.')
         thread.start()
 
     def queue_thread(self, thread, start, is_last_line):
         sublime.set_timeout(
-            lambda: self.handle_result(thread, start, is_last_line),
+            lambda: self.handle_thread(thread, start, is_last_line),
             100
         )
 
-    def handle_result(self, thread, next_start, is_last_line):
+    def handle_thread(self, thread, next_start, is_last_line):
         if thread.is_alive():
-            self.view.set_status(
-                'worksheet', 'Waiting for {0} REPL.'.format(self.language))
+            self.set_status('Waiting for %(language)s REPL.')
             self.queue_thread(thread, next_start, is_last_line)
         else:
-            self.view.set_status('worksheet', '')
+            self.set_status('')
             edit = self.view.begin_edit('process_line')
             self.view.insert(edit, next_start, thread.result)
             self.view.end_edit(edit)
             next_start += len(thread.result)
             if not is_last_line:
                 self.process_line(next_start)
+
+    def set_status(self, msg, key='worksheet'):
+        self.view.set_status(key, msg % {'language': self.get_language()})
