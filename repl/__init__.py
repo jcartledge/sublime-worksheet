e4859aaf03f3f3a66acef94ff3629c4d726c260f
Timon Wong <timon86.wang@gmail.com>
2013-07-02 16:10:28 +0800

    Attempt to make plugin compatible with both ST2 and ST3

--- repl/__init__.py
+++ repl/__init__.py
@@ -1,6 +1,18 @@
-from repl import get_repl
-from .repl import Repl
-from .repl import ReplResult
-from .repl import ReplStartError
-from .repl_thread import ReplThread
-import ftfy
+import sys
+
+PY3K = sys.version_info >= (3, 0, 0)
+
+if PY3K:
+    from . import ftfy
+    from .repl import get_repl
+    from .repl import Repl
+    from .repl import ReplResult
+    from .repl import ReplStartError
+    from .repl_thread import ReplThread
+else:
+    from repl import get_repl
+    from repl import Repl
+    from repl import ReplResult
+    from repl import ReplStartError
+    from repl_thread import ReplThread
+    import ftfy
